348fd8c27b46a1e06bb7ab841b8af35f2a01f58f
Taulib <2018118252@ub.edu.bz>
2023-03-23 16:48:47 -0600

    my json repsonses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,12 +3,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	//create a map to hold healthcheck data
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	// converting map into a json object
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The Server has ran into a error and could not meet your expectations", http.StatusInternalServerError)
+		return
+	}
+
+	// adding a new line to make the viewing easier
+	js = append(js, '\n')
+	// showing that we will serve our responses using json
+	w.Header().Set("Content-Type", "application/json")
+	// write the byte slice
+	w.Write(js)
+
 }
